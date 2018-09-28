1cf2f3b20a283f131b72ab295d7f0de825890f5d
Camila <camipereira2111@gmail.com>
2018-09-28 08:56:29 -0300

    Ordenando codigo

--- src/components/Muro/VistaMuro.js
+++ src/components/Muro/VistaMuro.js
@@ -2,22 +2,18 @@ import React, { Component } from 'react';
 import './vistaMuro.css';
 import Navbar from '../Common/Navbar';
 import Comentario from './Comentario';
-import Carrusel from '../Common/Carrusel';
 import './vistaMuro.css';
 
 
 
 
 class VistaMuro extends Component {
-  // constructor() {
-  //   super();
-  // }
+ 
   render() {
     return (
       <div className = "fondoMuro">
         <div>
           <Navbar/>
-          {/* <Carrusel/> */}
         </div>""
         <div className = "comentario">
           <Comentario/>
