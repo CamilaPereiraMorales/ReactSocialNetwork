3c8b457dce3a42033db1997f0311dd3ceb60cbb1
Camila <camipereira2111@gmail.com>
2018-09-28 09:08:20 -0300

    cambio de posicion

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ import Buscador from './components/buscadorClinicas/Buscador';
 class App extends Component {
   render() {
     return (
-      // <Buscador/>
+        // <VistaMuro/>
         <Router basename={process.env.PUBLIC_URL + '/'}>
           <div>
           <Route exact path="/" component={UsuarioAutenticado}/>
           <Route path="/registro" component={Registro}/>
           <Route path="/perfil" component={Perfil}/>
-          <Route path="/muro" component={VistaMuro}/>
           <Route path="/buscador" component={Buscador}/>
+          <Route path="/muro" component={VistaMuro}/>
           </div>
         </Router>
     
